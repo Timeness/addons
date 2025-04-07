938d08888f50a28105f7dc745fc82a16f300a702
Timeness <careless@flash.co>
2025-04-07 14:55:21 +0530

    done

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "Spectrum-v4";
+const urlsToCache = [
+  "/",
+  "/home.html",
+  "/manifest.json",
+  "/resources/Icons001.png",
+  "/resources/Icons002.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
